2cde950ed8153a8e3b1abebdf09b3b5776429394
fabiodev30 <fabiodev30@gmail.com>
2022-06-29 16:56:42 +0200

    remove parts not important

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,4 @@
-from django.urls import path
-
-from .views import TaskView,TaskDetailView,TaskCreateView,TaskUpdateView,TaskDeleteView,CustomLoginView,RegisterView
-from django.contrib.auth.views import LogoutView
+from django.urls import path,include
 
 urlpatterns = [
-    path('login/',CustomLoginView.as_view(),name='login'),
-    path('logout/',LogoutView.as_view(next_page='tasks'),name='logout'),
-    path('register/',RegisterView.as_view(),name='register'),
-    path('', TaskView.as_view(), name='tasks'),
-    path('task/<int:pk>/',TaskDetailView.as_view(), name='task'),
-    path('create-task/',TaskCreateView.as_view(), name='task-create'),
-    path('task-update/<int:pk>/',TaskUpdateView.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/',TaskDeleteView.as_view(), name='task-delete'),
 ]
