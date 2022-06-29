3dfa42e973f1b2d6d706be833133e7c113feea8a
fabiodev30 <fabiodev30@gmail.com>
2022-06-29 20:12:21 +0200

    add detail for every post

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
-from .views import MainpageView
+from .views import MainpageView,PostDetailView
 
+app_name = 'blog'
 urlpatterns = [
      path('', MainpageView.as_view(), name='home'),
+     path('post/<slug:slug>', PostDetailView.as_view(), name='post-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from .models import Post
 
 
 class MainpageView(ListView):
     template_name = "main-page/mainpage.html"
 
     def get_context(self):
+        posts= Post.objects.all()
         context = {
+            'posts':posts,
         }
         return context
 
     def get(self, request):
         return render(request, self.template_name, self.get_context())
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, slug=kwargs['slug'])
+        context = {'post': post}
+        return render(request, 'post-page/post-page.html', context)
