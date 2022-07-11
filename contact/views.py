ddf438123920e811167977dd5e47657222474c00
fabiodev30 <fabiodev30@gmail.com>
2022-07-11 11:30:01 +0200

    add contact page html

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact_view(request):
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
