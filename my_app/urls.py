9701ceba1cb1ab1a37e01c8c2b3b1d3ae27bbcb8
fabiodev30 <fabiodev30@gmail.com>
2022-07-11 11:13:57 +0200

    add contact app

--- my_app/urls.py
+++ my_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from contact import views as contact_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('contact/', contact_views.contact_view, name='contact'),
 ]
