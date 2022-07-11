7388045cc3b4fa56d6f59dd3ab44aedc8080a43d
fabiodev30 <fabiodev30@gmail.com>
2022-07-11 11:43:07 +0200

    fixed contact page

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import ContactForm
+from core.models import Category, Post
 
 
 def contact_view(request):
     form = ContactForm()
-    context = {'form': form}
+    categories = Category.objects.all()
+    latest_posts = Post.objects.all()[:2]
+    context = {
+        'form': form,
+        'categories': categories,
+        'latest_posts': latest_posts,
+
+    }
     return render(request, 'contact/contact.html', context)
