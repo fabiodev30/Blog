f04b7e975d2d4ad6905ce79bc4840c0f0a45aff5
fabiodev30 <fabiodev30@gmail.com>
2022-07-12 18:48:03 +0200

    add card to blog page

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import AboutView, MainpageView,PostDetailView
+from .views import AboutView, MainpageView,PostDetailView, SearchResultsView
 from django.conf.urls.static import static
 from django.conf import settings
 
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from requests import post
 from .models import Category, Post
 
 
@@ -45,11 +46,15 @@ class AboutView(ListView):
 
 
 class SearchResultsView(ListView):
-    template_name = 'saerch_results/search_results.html'
+    template_name = 'search_results/search_results.html'
     def get_context(self):
+        query = self.request.GET.get("q")
+        object_list = Post.objects.filter(title__icontains=query)
         categories=Category.objects.all()
         latest_posts=Post.objects.all()[:2]
         context = {
+            'query': query,
+            'post_searched':object_list,
             'categories': categories,
             'latest_posts': latest_posts
         }
