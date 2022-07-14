116da6efc06ad6abcdb9deac0154abfe19cc4551
fabiodev30 <fabiodev30@gmail.com>
2022-07-14 16:44:17 +0200

    Adjust buildpacks on Heroku

--- my_app/settings.py
+++ my_app/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS= [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/src"),
