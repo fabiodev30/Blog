d79a97ded38bccdbdaf3f18719fefb4a1c19293f
fabiodev30 <fabiodev30@gmail.com>
2022-07-15 13:07:09 +0200

    add heroku settings db

--- my_app/settings.py
+++ my_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,7 @@ MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/src/assets/img/')
 
 STATIC_ROOT = "collectstatic/"
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
