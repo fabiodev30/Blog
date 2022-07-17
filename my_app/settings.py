209b607a48d94870403f2462407e1ea890481a40
fabiodev30 <fabiodev30@gmail.com>
2022-07-17 18:56:23 +0200

    add whitenoise

--- my_app/settings.py
+++ my_app/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'my_app.urls'
@@ -162,3 +163,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/src/assets/img/')
 
 STATIC_ROOT = "collectstatic/"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
